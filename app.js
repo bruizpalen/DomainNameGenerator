a21edbf87a159a1eb41844c669b523a7d4bed7ba
Berta Ruiz <bruizpalenzuela@gmail.com>
2023-05-11 14:12:35 +0200

    Added let and ;

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ let tail = ['.com', '.net', '.us', '.io']
 // createString function can fill a new Array with each of the final domain name and/or it can print it on the console
 function createString(pron, adjec, name, extension){
     let newArray = []
-    for (i=0; i<pron.length; i++){
-        for (j=0; j<adjec.length; j++){
-            for (k=0; k<name.length; k++){
-                for (let l = 0; l<extension.length; l++){
+    for (let i=0; i<pron.length; i++){
+        for (let j=0; j<adjec.length; j++){
+            for (let k=0; k<name.length; k++){
+                for (let l=0; l<extension.length; l++){
                     // newArray.push(`${pron[i]}${adjec[j]}${name[k]}${extension[l]}`)
-                    console.log(`${pron[i]}${adjec[j]}${name[k]}${extension[l]}`)
+                    console.log(`${pron[i]}${adjec[j]}${name[k]}${extension[l]}`);
                 
                 
                 }
@@ -25,4 +25,4 @@ function createString(pron, adjec, name, extension){
 
 
 // Calling the function to see all the domain names
-createString(pronoun, adj, noun, tail)
+createString(pronoun, adj, noun, tail);
